50209de34a3676c88f76710aaa18486d13ead7fd
Pospelov Vladimir <pospelovvladimir1993@yandex.ru>
2025-10-26 15:32:29 +0500

    add base logic in GameCell component

--- src/components/GameCell/GameCell.tsx
+++ src/components/GameCell/GameCell.tsx
@@ -1,7 +1,39 @@
 import "./gameCell.scss"
+import { useState, type FC, useEffect } from "react"
 
-const GameCell = () => {
-  return <div className="game-cell">GameCell</div>
+type TVariantCell = "red" | "blue" | "green" | "violet"
+
+type TGameCell = {
+  variant: TVariantCell
+}
+
+const GameCell: FC<TGameCell> = ({ variant }) => {
+  const [stone, setStone] = useState<string>("")
+
+  const getStone = async (variant: TVariantCell) => {
+    switch (variant) {
+      case "red":
+        return (await import("../../assets/images/t1.png")).default
+      case "blue":
+        return (await import("../../assets/images/t2.png")).default
+      case "green":
+        return (await import("../../assets/images/t3.png")).default
+      case "violet":
+        return (await import("../../assets/images/t4.png")).default
+      default:
+        throw new Error("Unknown variant")
+    }
+  }
+
+  useEffect(() => {
+    ;(async () => setStone(await getStone(variant)))()
+  }, [variant])
+
+  return (
+    <div className="game-cell">
+      <img className="game-cell__img" src={stone} />
+    </div>
+  )
 }
 
 export default GameCell
