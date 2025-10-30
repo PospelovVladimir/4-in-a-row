9d0b6bc26e6ffc5b680c0ae324dc0437b98f6a55
Pospelov Vladimir <pospelovvladimir1993@yandex.ru>
2025-10-30 16:33:44 +0500

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./App.scss"
 import GameBoard from "./components/GameBoard/GameBoard"
+import Header from "./components/Header/Header"
 
 const App = () => {
-  return <div className="app"><GameBoard></GameBoard></div>
+  return (
+    <div className="app">
+      <div className="container">
+        <Header />
+        <GameBoard />
+      </div>
+    </div>
+  )
 }
 
 export default App
