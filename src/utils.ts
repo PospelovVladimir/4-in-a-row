9a3773e97e491df8c23813a503c885334298a23a
Pospelov Vladimir <pospelovvladimir1993@yandex.ru>
2025-11-02 18:28:26 +0500

    added checkBoardFull function

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,14 @@ export const chooseChipExcludingSelected = (arr: TVariantCell[], selectedVariant
     const partColors = arr.filter((el) => el !== selectedVariant)
     return partColors[Math.floor(Math.random() * partColors.length)]
 }
+
+export const checkBoardFull = (grid: (TVariantCell | null)[][]): boolean => {
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[r].length; c++) {
+      if (grid[r][c] === null) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
